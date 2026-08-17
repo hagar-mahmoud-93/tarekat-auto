8114a9cedb07fb4a0baac3b89ce068233a8990d0
Lubna <lubna.qaqeesh@sitech.me>
2026-08-17 12:27:37 +0300

    Wait loader to disappear in terms and conditions page

--- pages/heirs-determination-pages/terms-and-conditions.page.ts
+++ pages/heirs-determination-pages/terms-and-conditions.page.ts
@@ -1,10 +1,15 @@
 import { BasePage } from '../base.page';
 import { TermsAndConditionsLocators } from '../../locators/heirs-determination-locators/terms-and-conditions.locators';
+import { waitForLoadingOverlayToDisappear } from '../../steps/wait-for-loading-overlay';
 
 export class TermsAndConditionsPage extends BasePage {
   private readonly locators = new TermsAndConditionsLocators(this.page);
 
+  /** Advancing to بيانات مقدم الطلب can show the wizard's transitional loading overlay, same as
+   *  other step-advancing actions in this flow (see MainMenuPage.openMyOrders,
+   *  HeirsListPage.acknowledgeHeirsList) - wait it out before returning. */
   async agree() {
     await this.locators.agreeButton().click();
+    await waitForLoadingOverlayToDisappear(this.page);
   }
 }
